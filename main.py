520ebbbe15f0b07611f25f995f6f18e778bc2c2d
aholt@rsmas.miami.edu <aholt@rsmas.miami.edu>
2026-05-15 13:57:10 -0400

    basic data read-in

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from pathlib import Path
+from src.functions import load_data, clean_data, describe_data, describe_water_quality
+
+DATA_PATH = "data/raw/OE Data Periods 1-4.xlsx"
+
+
+def main():
+    print(f"Loading data from: {DATA_PATH}\n")
+    df_raw = load_data(DATA_PATH)
+    df = clean_data(df_raw)
+
+    describe_data(df)
+    describe_water_quality(df)
+
+if __name__ == "__main__":
+    main()
